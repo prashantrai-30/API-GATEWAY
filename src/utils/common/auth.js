12036726fdd0365017e3063d86ea2d95b6d05c90
prashant <prashantkumar2488@gmail.com>
2024-07-13 16:12:09 +0530

    Added signin with jwt creation

---
+++ src/utils/common/auth.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt');
+
+const jwt = require('jsonwebtoken');
+
+const {ServerConfig} = require('../../config');
+const serverConfig = require('../../config/server-config');
+function checkPassword(plainPassword, encryptPassword) {
+    try {
+        return bcrypt.compareSync(plainPassword,encryptPassword);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+function createToken(input) {
+    try {
+        return jwt.sign(input,serverConfig.JWT_SECRET, {expiresIn:serverConfig.JWT_EXPIRY});
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = {
+    checkPassword,
+    createToken
+}
