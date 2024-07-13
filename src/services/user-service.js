022e3091f1bd55b04d094d95e65ca83d873086cb
prashant <prashantkumar2488@gmail.com>
2024-07-13 17:54:14 +0530

    Added jwt token expire handling

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -56,6 +56,9 @@ async function isAuthenticated(token) {
         if(error.name == 'JsonWebTokenError') {
             throw new AppError('Invalid JWT token',StatusCodes.BAD_REQUEST);
         }
+        if(error.name == 'TokenExpiredError') {
+            throw new AppError('JWT token Expired',StatusCodes.BAD_REQUEST);
+        }
         console.log(error);
         throw new Apperror('Something went wrong',StatusCodes.INTERNAL_SERVER_ERROR);
     }
